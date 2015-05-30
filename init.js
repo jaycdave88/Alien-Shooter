490e4c5716ce3f44d3919b1d717d98694b767729
Jay Dave <jaycdave@gmail.com>
2015-05-30 15:36:35 -0700

    added start button into modal and added sprite death

--- init.js
+++ init.js
@@ -8,9 +8,10 @@ var alienImage;
 var stage;
 var animation;
 var deathAnimation;
+var deathSprite;
 var spriteSheet;
-var enemyXPos=100;
-var enemyYPos=100;
+var enemyXPos = 100;
+var enemyYPos = 100;
 var enemyXSpeed = 1.5;
 var enemyYSpeed = 1.75;
 var score = 0;
@@ -19,8 +20,8 @@ var gameTimer;
 var gameTime = 0;
 var timerText;
 
-function startGame(){
-  
+function startGame() {
+
     var canvas = document.getElementById('myCanvas');
     context = canvas.getContext('2d');
     context.canvas.width = WIDTH;
@@ -32,30 +33,45 @@ function startGame(){
     queue.on("complete", queueLoaded, this);
     createjs.Sound.alternateExtensions = ["ogg"];
 
-    queue.loadManifest([
-        {id: 'backgroundImage', src: 'assets/background.png'},
-        {id: 'crossHair', src: 'assets/crosshair.png'},
-        {id: 'shot', src: 'assets/shot.mp3'},
-        {id: 'background', src: 'assets/space.mp3'},
-        {id: 'gameOverSound', src: 'assets/gameOver.mp3'},
-        {id: 'tick', src: 'assets/tick.mp3'},
-        {id: 'deathSound', src: 'assets/die.mp3'},
-        {id: 'alienSpriteSheet', src: 'assets/space.png'},
-        {id: 'alienDeath', src: 'assets/alienDeath.png'},
-    ]);
+    queue.loadManifest([{
+        id: 'backgroundImage',
+        src: 'assets/background.png'
+    }, {
+        id: 'crossHair',
+        src: 'assets/crosshair.png'
+    }, {
+        id: 'shot',
+        src: 'assets/shot.mp3'
+    }, {
+        id: 'background',
+        src: 'assets/space.mp3'
+    }, {
+        id: 'gameOverSound',
+        src: 'assets/gameOver.mp3'
+    }, {
+        id: 'tick',
+        src: 'assets/tick.mp3'
+    }, {
+        id: 'deathSound',
+        src: 'assets/die.mp3'
+    }, {
+        id: 'alienSpriteSheet',
+        src: 'assets/space.png'
+    }, {
+        id: 'alienDeath',
+        src: 'assets/alienDeath.png'
+    }, ]);
     queue.load();
     gameTimer = setInterval(updateTime, 1000);
-
-$("#start").hide();
-$("#again").show();
-$(".info").hide();
 }
 
-function queueLoaded(event){
+function queueLoaded(event) {
     var backgroundImage = new createjs.Bitmap(queue.getResult("backgroundImage"))
     stage.addChild(backgroundImage);
 
-    createjs.Sound.play("background", {loop: -1});
+    createjs.Sound.play("background", {
+        loop: -1
+    });
 
     scoreText = new createjs.Text("+Points " + score.toString(), "26px Arial", "#FFF");
     scoreText.x = 50;
@@ -67,20 +83,31 @@ function queueLoaded(event){
     timerText.y = 450;
     stage.addChild(timerText);
 
-  
+
     spriteSheet = new createjs.SpriteSheet({
         "images": [queue.getResult('alienSpriteSheet')],
-        "frames": {"width": 100, "height": 115},
-        "animations": { "invade": [0] }
+        "frames": {
+            "width": 100,
+            "height": 115
+        },
+        "animations": {
+            "invade": [0, 3]
+        }
     });
 
-    notInUse = new createjs.SpriteSheet({
-    	"images": [queue.getResult('alienDeath')],
-    	"frames": {"width": 198, "height" : 148},
-    	"animations": {"die": [0,5, false,1 ] }
+    deathSprite = new createjs.SpriteSheet({
+        "images": [queue.getResult('alienDeath')],
+        "frames": {
+            "width": 118,
+            "height": 118
+        },
+        "animations": {
+            "die": [0, 5, false]
+        }
     });
 
     createEnemy();
+
 
     createjs.Ticker.setFPS(15);
     createjs.Ticker.addEventListener('tick', stage);
@@ -89,65 +116,59 @@ function queueLoaded(event){
     window.onmousedown = handleMouseDown;
 }
 
-function createEnemy()
-{
-	animation = new createjs.Sprite(spriteSheet, "invade");
+function createEnemy() {
+    animation = new createjs.Sprite(spriteSheet, "invade");
     animation.regX = 49;
     animation.regY = 58;
     animation.x = enemyXPos;
     animation.y = enemyYPos;
     animation.gotoAndPlay("invade");
-    stage.addChildAt(animation,1);
+    stage.addChildAt(animation, 1);
 }
 
-function alienDeath()
-{
-  deathAnimation = new createjs.Sprite(notInUse, "die");
-  deathAnimation.regX = 99;
-  deathAnimation.regY = 58;
-  deathAnimation.x = enemyXPos;
-  deathAnimation.y = enemyYPos;
-  deathAnimation.gotoAndPlay("die");
-  stage.addChild(deathAnimation);
+function alienDeath() {
+    deathAnimation = new createjs.Sprite(deathSprite, "die");
+    deathAnimation.regX = 49;
+    deathAnimation.regY = 58;
+    deathAnimation.x = enemyXPos;
+    deathAnimation.y = enemyYPos;
+    deathAnimation.gotoAndPlay("die");
+    stage.addChild(deathAnimation);
 }
 
-function tickEvent()
-{
-	if(enemyXPos < WIDTH && enemyXPos > 0)
-	{
-		enemyXPos += enemyXSpeed;
-	} else 
-	{
-		enemyXSpeed = enemyXSpeed * (-1);
-		enemyXPos += enemyXSpeed;
-	}
-	if(enemyYPos < HEIGHT && enemyYPos > 0)
-	{
-		enemyYPos += enemyYSpeed;
-	} else
-	{
-		enemyYSpeed = enemyYSpeed * (-1);
-		enemyYPos += enemyYSpeed;
-	}
+function tickEvent() {
+    if (enemyXPos < WIDTH && enemyXPos > 0) {
+        enemyXPos += enemyXSpeed;
+    } else {
+        enemyXSpeed = enemyXSpeed * (-1);
+        enemyXPos += enemyXSpeed;
+    }
+    if (enemyYPos < HEIGHT && enemyYPos > 0) {
+        enemyYPos += enemyYSpeed;
+    } else {
+        enemyYSpeed = enemyYSpeed * (-1);
+        enemyYPos += enemyYSpeed;
+    }
 
-	animation.x = enemyXPos;
-	animation.y = enemyYPos;
+    animation.x = enemyXPos;
+    animation.y = enemyYPos;
 
-	
+
 }
 
-function handleMouseDown(event)
-{
+function handleMouseDown(event) {
     createjs.Sound.play("shot");
 
     enemyXSpeed *= 1.05;
     enemyYSpeed *= 1.06;
-    
+
     crossHair = new createjs.Bitmap(queue.getResult("crossHair"));
-    crossHair.x = event.clientX-45;
-    crossHair.y = event.clientY-45;
+    crossHair.x = event.clientX - 45;
+    crossHair.y = event.clientY - 45;
     stage.addChild(crossHair);
-    createjs.Tween.get(crossHair).to({alpha: 0},1000);
+    createjs.Tween.get(crossHair).to({
+        alpha: 0
+    }, 1000);
 
     var shotX = Math.round(event.clientX);
     var shotY = Math.round(event.clientY);
@@ -157,43 +178,37 @@ function handleMouseDown(event)
     var distX = Math.abs(shotX - spriteX);
     var distY = Math.abs(shotY - spriteY);
 
-    if(distX < 20 && distY < 29 )
-    {
-    	stage.removeChild(animation);
-    	alienDeath();
-    	score += 100;
-    	scoreText.text = "+Points " + score.toString();
-    	createjs.Sound.play("deathSound");
-    	
-    	enemyYSpeed *= 1.25;
-    	enemyXSpeed *= 1.3;
+    if (distX < 20 && distY < 29) {
+        stage.removeChild(animation);
+        alienDeath();
+        score += 100;
+        scoreText.text = "+Points " + score.toString();
+        createjs.Sound.play("deathSound");
 
-    	var timeToCreate = Math.floor((Math.random()*3500)+1);
-	    setTimeout(createEnemy,timeToCreate);
+        enemyYSpeed *= 1.25;
+        enemyXSpeed *= 1.3;
 
-    } else
-    {
-    	score -= 30;
-    	scoreText.text = "+Points " + score.toString();
+        var timeToCreate = Math.floor((Math.random() * 3500) + 1);
+        setTimeout(createEnemy, timeToCreate);
+
+    } else {
+        score -= 30;
+        scoreText.text = "+Points " + score.toString();
     }
 }
 
-function updateTime()
-{
-	gameTime += 1;
-	if(gameTime > 60)
-	{
-		timerText.text = "Game Over";
-		stage.removeChild(animation);
-		stage.removeChild(crossHair);
+function updateTime() {
+    gameTime += 1;
+    if (gameTime > 3) {
+        timerText.text = "Game Over";
+        stage.removeChild(animation);
+        stage.removeChild(crossHair);
         createjs.Sound.removeSound("background");
-        var si =createjs.Sound.play("gameOverSound");
-		clearInterval(gameTimer);
-	}
-	else
-	{
-		timerText.text = "Time: " + gameTime
-    createjs.Sound.play("tick");
-	}
+        var si = createjs.Sound.play("gameOverSound");
+        clearInterval(gameTimer);
+        $("#again").show();
+    } else {
+        timerText.text = "Time: " + gameTime
+        createjs.Sound.play("tick");
+    }
 }
-
